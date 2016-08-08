a700dfe52e35242c75edacd3fd8034805db9b7d6
Clément Gillard <clement.gillard@epita.fr>
2016-08-08 17:50:34 +0200

    Add word types

---
+++ words.py
@@ -0,0 +1,13 @@
+from base import Node
+
+class Cmavo(Node):
+    '''Abstract class for cmavo.'''
+    pass
+
+class Brivla(Node):
+    '''Abstract class for brivla.'''
+    pass
+
+class Sumti(Node):
+    '''Abstract class for sumti.'''
+    pass
