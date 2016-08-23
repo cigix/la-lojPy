70f103c729d92c05b16ed69c7e2b14518e276588
Clément Gillard <clement.gillard@epita.fr>
2016-08-24 00:01:22 +0200

    Add brivla description
    Add description to edges when printing a brivla in Graphviz DOT
    format. The description of x1 is always "x1", then descriptions for
    x2 -> x5 are given through the _function list attribute. When x2
    exists, nodes for x1 and x2 are separated by an invisible node for
    clarity.

--- words.py
+++ words.py
@@ -6,7 +6,24 @@ class Cmavo(Node):
 
 class Brivla(Node):
     '''Abstract class for brivla.'''
-    pass
+    _functions = []
+    def toDot(self, ID, out):
+        ID += 1
+        myID = ID
+        print("\t{} [label = {}]".format(ID, self.__class__.__name__),
+              file=out)
+        print("\t{} -> {} [label = x1]".format(myID, ID + 1), file=out)
+        ID = self.children[0].toDot(ID, out)
+        if 1 < len(self.children):
+            ID += 1
+            print("\t{} -> {} [color = white]".format(myID, ID),
+                  "\t{} [color = white, label = \"\"]".format(ID),
+                  sep="\n", file=out)
+            for c, f in zip(self.children[1:], self._functions):
+                print("\t{} -> {} [label = \"{}\"]".format(myID, ID + 1, f),
+                      file=out)
+                ID = c.toDot(ID, out)
+        return ID
 
 class Sumti(Node):
     '''Abstract class for sumti.'''
